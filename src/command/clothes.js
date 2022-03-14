10c5c8fa848aff642e1eadbee87b6311589fda06
erick1548 <43975080+erick1548@users.noreply.github.com>
2022-03-14 16:19:01 -0300

    Add files via upload

--- src/command/clothes.js
+++ src/command/clothes.js
@@ -8,12 +8,12 @@ async function parse (txt) {
 }
 
 async function handle () {
-  const txt = await fetchText(`https://images.habbo.com/gordon/${config.prod}/figuremapv2.xml`)
+  const txt = await fetchText(`https://assets.atlantaserver.me/gordon/PRODUCTION-201709192204-203982673/figuremap.xml`)
   const all = await parse(txt)
 
   await fetchMany([...all].map((item) => {
     return {
-      src: `https://images.habbo.com/gordon/${config.prod}/${item}.swf`,
+      src: `https://assets.atlantaserver.me/gordon/PRODUCTION-201709192204-203982673/${item}.swf`,
       dst: `gordon/${config.prod}/${item}.swf`
     }
   }))
